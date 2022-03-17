2a9336745d21d45987c29d36f3e0411193e53145
Matthieu Chouanneau <m.chouanneau@gmail.com>
2022-03-17 15:53:25 +0100

    rails watch list done

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -7,16 +7,17 @@ class BookmarksController < ApplicationController
 
   def create
     @bookmark = Bookmark.new(bookmark_params)
-    @bookmark.list_id = @bookmark
+    @list = List.find(params[:list_id])
+    @bookmark.list = @list
     if @bookmark.save
-    redirect_to bookmark_path(@article)
+      redirect_to list_path(@list)
     else
-    render :new
+      render 'lists/new'
     end
   end
 
   def destroy
-    @bookmark = Bookmark.find(bookmark_params)
+    @bookmark = Bookmark.find(params[:id])
     @bookmark.destroy
     redirect_to bookmark_path
   end
@@ -24,7 +25,7 @@ class BookmarksController < ApplicationController
   private
 
   def bookmark_params
-    params.require(:bookmark).permit(:comment, :movie_id, :list_id)
+    params.require(:bookmark).permit(:comment, :movie_id)
   end
 
 end
