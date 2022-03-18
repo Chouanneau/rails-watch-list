1e7f2df8b3f77378a976a16b1fe8af98f88a5ab7
Matthieu Chouanneau <m.chouanneau@gmail.com>
2022-03-18 12:09:58 +0100

    rails watch qui fonctionne

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -6,20 +6,19 @@ class BookmarksController < ApplicationController
   end
 
   def create
-    @bookmark = Bookmark.new(bookmark_params)
     @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new(bookmark_params)
     @bookmark.list = @list
     if @bookmark.save
       redirect_to list_path(@list)
     else
-      render 'lists/new'
+      render :new
     end
   end
 
   def destroy
-    @bookmark = Bookmark.find(params[:id])
     @bookmark.destroy
-    redirect_to bookmark_path
+    redirect_to list_path(@bookmark.list)
   end
 
   private
@@ -27,5 +26,4 @@ class BookmarksController < ApplicationController
   def bookmark_params
     params.require(:bookmark).permit(:comment, :movie_id)
   end
-
 end
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,12 +4,12 @@ class ListsController < ApplicationController
     @lists = List.all
   end
 
-  def new
-    @list = List.new
-  end
-
   def show
     @list = List.find(params[:id])
+  end
+
+  def new
+    @list = List.new
   end
 
   def create
@@ -21,10 +21,15 @@ class ListsController < ApplicationController
     end
   end
 
+  def destroy
+    @list = List.find(params[:id])
+    @list.destroy
+    redirect_to lists_path
+  end
+
   private
 
   def list_params
     params.require(:list).permit(:name)
   end
-
 end
